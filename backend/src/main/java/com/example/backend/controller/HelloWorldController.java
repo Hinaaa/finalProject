83324f709c17657b9635a1886c1f462a995cf7af
hinajawaid <hinajawaid.h@gmail.com>
2025-05-13 11:43:24 +0200

    App.tsx updated with frontend changes add sonar setup

--- backend/src/main/java/com/example/backend/controller/HelloWorldController.java
+++ backend/src/main/java/com/example/backend/controller/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.example.backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173") //frontend (Vite) is running on port 5173 and your backend on 8080, this is a cross-origin request,
 @RequestMapping("/api")
 public class HelloWorldController {
     @GetMapping("/hello")
